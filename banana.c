70b97dd0d84c8966eb481a3e1c532012743700d3
Huining Liu <huiningliu@Huinings-MacBook-Pro.local>
2021-03-14 15:21:58 -0400

    added a new function

--- banana.c
+++ banana.c
@@ -1,8 +1,14 @@
-
 #include <stdio.h>
+
+void print_numbers(void) {
+for (int i = 0; i <= 10; i++) {
+	printf("%d",i);
+}
+}
+
 int main(void) {
 	printf("Hello World!\n");
 	printf("This is a new line ...\n");
+	print_numbers();
+	
 }
-
- remove this line
